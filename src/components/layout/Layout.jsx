f64672a40c13d6a3139a10c9fc52e7ed43ca2321
noss101 <elkhayatinosair@gmail.com>
2025-06-18 14:57:37 +0100

    Add semi-transparent background image

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,9 +4,14 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-br from-blue-50 to-indigo-50">
+    <div className="relative flex flex-col min-h-screen bg-gradient-to-br from-blue-50 to-indigo-50">
+      {/* Background image */}
+      <div
+        aria-hidden="true"
+        className="absolute inset-0 bg-[url('/images/zellige.jpg')] bg-cover bg-center opacity-30 pointer-events-none"
+      ></div>
       <Navbar />
-      <main className="flex-grow py-8">
+      <main className="relative flex-grow py-8">
         {children}
       </main>
       <Footer />
