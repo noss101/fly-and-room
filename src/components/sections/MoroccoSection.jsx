3763156188b76db163a77093114c53b8f84f68a5
noss101 <elkhayatinosair@gmail.com>
2025-06-20 22:14:59 +0100

    style: update morocco card background and title

--- src/components/sections/MoroccoSection.jsx
+++ src/components/sections/MoroccoSection.jsx
@@ -7,15 +7,15 @@ const MoroccoSection = () => {
   return (
     <section className="py-16">
       <div className="container mx-auto px-4 space-y-8">
-        <div className="bg-white/70 backdrop-blur-md rounded-3xl shadow-2xl p-8 text-center">
+        <div className="bg-[#272724] rounded-3xl shadow-2xl p-8 text-center text-white">
           <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 text-center">
-            Find Cheap Flights and Hotels in
+            Find Cheap Flights and Hotels in{' '}
             <span className="inline-flex items-center">
-             Morocco
+              Morocco
               <img src="/icons/moroccoflag.png" alt="Morocco flag" className="w-6 h-6 ml-2 inline-block" />
             </span>
           </h2>
-          <p className="text-xl max-w-3xl mx-auto text-gray-700">
+          <p className="text-xl max-w-3xl mx-auto text-gray-300">
             Explore Morocco with great deals on flights and accommodations. Search for affordable travel options and compare prices on top hotels in Marrakech, Casablanca, and beyond.
           </p>
           <Link
