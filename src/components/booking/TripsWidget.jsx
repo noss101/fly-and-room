cea483cf725bfa4a9d7eb6a91ba02305d9c7b4e0
noss101 <elkhayatinosair@gmail.com>
2025-06-20 18:39:17 +0100

    Update TripsWidget script src

--- src/components/booking/TripsWidget.jsx
+++ src/components/booking/TripsWidget.jsx
@@ -7,13 +7,9 @@ const TripsWidget = () => {
 
     const script = document.createElement('script');
     script.src =
-      'https://widgets.tiqets.com/content?currency=USD&trs=425059&shmarker=636307.636307&language=en&layout=responsive&cards=4&powered_by=true&campaign_id=89&promo_id=3947';
+      'https://tpwidg.com/content?currency=USD&trs=425059&shmarker=636307.636307&product=1106931%2C1106930%2C1114535%2C1114028&language=en&layout=horizontal&powered_by=true&campaign_id=89&promo_id=3948';
     script.async = true;
     script.charset = 'utf-8';
-    script.crossOrigin = 'anonymous';
-    script.onerror = () => {
-      container.textContent = 'Failed to load trips.';
-    };
 
     container.appendChild(script);
 
