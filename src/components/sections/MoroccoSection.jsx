dbc8cfebfb5cd32b9da705cfcfc10a02e53b3183
noss101 <elkhayatinosair@gmail.com>
2025-06-21 01:52:06 +0100

    style: update Our Partners section

--- src/components/sections/MoroccoSection.jsx
+++ src/components/sections/MoroccoSection.jsx
@@ -69,28 +69,28 @@ const MoroccoSection = () => {
         </p>
         <ServicesSection />
       </div>
-      <div className="bg-[#272724] shadow-2xl p-8 text-center text-white w-screen relative left-1/2 right-1/2 ml-[-50vw] mr-[-50vw]">
-        <h3 className="text-xl sm:text-2xl md:text-3xl font-bold mb-4">Our Partners</h3>
+      <div className="bg-[#F6F7EA] shadow-2xl p-8 text-center w-screen relative left-1/2 right-1/2 ml-[-50vw] mr-[-50vw]">
+        <h3 className="text-xl sm:text-2xl md:text-3xl font-bold mb-4 text-[#272724]">Our Partners</h3>
         <div className="flex justify-center items-center space-x-6">
           <img
             src="/icons/esimparicon.png"
             alt="eSIM partner"
-            className="h-12 sm:h-14 md:h-16 w-auto"
+            className="h-16 sm:h-20 md:h-24 w-auto"
           />
           <img
             src="/icons/localrenticonpar.png"
             alt="LocalRent partner"
-            className="h-12 sm:h-14 md:h-16 w-auto"
+            className="h-16 sm:h-20 md:h-24 w-auto"
           />
           <img
             src="/icons/pickupsparicon.png"
             alt="Pickups partner"
-            className="h-12 sm:h-14 md:h-16 w-auto"
+            className="h-16 sm:h-20 md:h-24 w-auto"
           />
           <img
             src="/icons/tripiconpar.png"
             alt="Trip partner"
-            className="h-12 sm:h-14 md:h-16 w-auto"
+            className="h-16 sm:h-20 md:h-24 w-auto"
           />
         </div>
       </div>
